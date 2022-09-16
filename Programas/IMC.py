4d07269701a59d79f498b15d0fe506114d5771f4
Fernando Morales <f.m199698@gmail.com>
2022-09-16 16:14:50 -0500

    Agregando Primer Programa que calcula el IMC

---
+++ Programas/IMC.py
@@ -0,0 +1,6 @@
+print('Programa que mide IMC\n****************************')
+print('Por favor, coloca los datos que se solicitan')
+altura = float(input('Coloca tu altura en m: '))
+peso = int (input('Coloca tu peso en KG:'))
+_IMC = round(peso/(altura**2),2)
+print('Tu IMC es de ', _IMC)
