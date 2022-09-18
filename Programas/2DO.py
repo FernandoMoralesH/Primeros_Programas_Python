9e0f9768b14ce09285458570067ef7a3164cd2b9
Fernando Morales <f.m199698@gmail.com>
2022-09-18 15:46:05 -0500

    Creando segundo programa que muestre un numero mayor de dos numeros dados

--- Programas/2DO.py
+++ Programas/2DO.py
@@ -1,1 +1,10 @@
-"""Segundo Programa:"""
+"""Segundo Programa:Identificar el numero Mayor"""
+#Variables
+
+num1= int(input("Escribe un numero entero: "))
+num2=  int(input("Escribe un numero entero: "))
+
+if num1 >num2:
+    print("El numero mayor entre " , num1, "y ", num2, "es: ", num1)
+else:
+    print("El numero mayor entre " , num1, "y ", num2, "es: ", num2)
