1bebc55a0c076d77b4484757fb2520ad7b77035c
Fernando Morales <f.m199698@gmail.com>
2022-09-18 15:50:36 -0500

    Agregando dos lineas al codigo para obtener un resultado si ambos numeros son iguales

--- Programas/2DO.py
+++ Programas/2DO.py
@@ -6,5 +6,7 @@ num2=  int(input("Escribe un numero entero: "))
 
 if num1 >num2:
     print("El numero mayor entre " , num1, "y ", num2, "es: ", num1)
+elif num2>num1:
+ print("El numero mayor entre " , num1, "y ", num2, "es: ", num2)
 else:
-    print("El numero mayor entre " , num1, "y ", num2, "es: ", num2)
+    print("Ambos numeros son iguales")
